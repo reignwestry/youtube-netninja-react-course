16a07c8c95ba75763cba4e8e59bf2adccebb4c5d
Reign <reignzj.westry@gmail.com>
2023-12-04 19:25:30 -0500

    Added handleDelete function to current branch

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,9 +1,8 @@
 import { useState } from "react";
-import BlogList from './BlogList';
+import BlogList from "./BlogList";
 
 const Home = () => {
-
-  const [blogs, setBlogs] = useState ([
+  const [blogs, setBlogs] = useState([
     {
       title: "My new website",
       body: "lorem ipsum ...",
@@ -30,14 +29,24 @@ const Home = () => {
     },
   ]);
 
+  const handleDelete = (id) => {
+    //Filters out all blogs not equal to the specified blog
+    const newBlogs = blogs.filter((blog) => blog.id !== id);
+    setBlogs(newBlogs); // saves the new blog list only to the current state
+  };
+
   //Passing blogs to the child component
   return (
     <div className="home">
-      <BlogList blogs={blogs} title="All Blogs!" />
+      <BlogList blogs={blogs} title="All Blogs!" handleDelete={handleDelete} />
       {/* //note Filter filters all elements that match the blog author mario */}
-      <BlogList blogs={blogs.filter((blog)=> blog.author === 'mario' )} title="Mario's blogs" />
-      </div>
-    )
+      <BlogList
+        blogs={blogs.filter((blog) => blog.author === "mario")}
+        title="Mario's blogs"
+        handleDelete={handleDelete}
+      />
+    </div>
+  );
 };
 
 export default Home;
